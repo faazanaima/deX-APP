c90b6fab4ac49f71815e42f57aa38a3e1438657c
Faaza Naima <ifafaaza@gmail.com>
2025-05-26 07:26:40 +0700

    Update conn.py

--- conn.py
+++ conn.py
@@ -5,9 +5,13 @@ import streamlit as st
 def get_connection():
     try:
         return psycopg2.connect(
-            st.secrets["DATABASE_URL"],
-            sslmode="require"
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            host=st.secrets["DB_HOST"],
+            port=st.secrets["DB_PORT"],
+            dbname=st.secrets["DB_NAME"]
         )
+
     except OperationalError as e:
         st.error(f"❌ Database connection failed: {e}")
         return None
